a4165e7df4d12112982ff747b878bdfb0c42b91f
Nupur Jaiswal <njais2@uis.edu>
2018-04-21 22:21:21 -0700

    Add code for "Split the string in tokens"

---
+++ src/main/java/com/nupurjaiswal/string/Token.java
@@ -0,0 +1,25 @@
+package com.nupurjaiswal.string;
+
+import java.util.Scanner;
+
+/*
+Given a string, , matching the regular expression [A-Za-z !,?._'@]+, split the string into tokens.
+We define a token to be one or more consecutive English alphabetic letters.
+ Then, print the number of tokens, followed by each token on a new line
+ */
+public class Token {
+
+    public static void main(String[] args) {
+
+        String s = "He is a very very good boy, isn't he?";
+        // Write your code here.
+        String[] inputArray = s.split("[\\s!,?._'@]+");
+        System.out.println(inputArray.length);
+        for(int i = 0; i < inputArray.length; i ++){
+            System.out.println(inputArray[i]);
+
+        }
+
+
+    }
+}
